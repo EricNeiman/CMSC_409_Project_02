1534de4b90f6f26156662eeff9e588ded924bec2
Eric Neiman <ericneiman25@gmail.com>
2018-11-26 19:26:24 -0500

    Updated word processing

--- Sentence_Processor.py
+++ Sentence_Processor.py
@@ -1,14 +1,15 @@
 import re
+import Porter_Stemmer_Python
+import ctypes as ctypes
 
 class SentenceProcessor:
     def __init__(self):
         self.sentences = open("sentences.txt", "r", encoding="utf8")  # sets up file to read from
-        self.processed = open("processed.txt", "w+")  # creates file to hold processed sentences
-        self.stop_words = open("stop_words.txt", "r")
+        self.processed = open("processed.txt", "w+", encoding="utf8")  # creates file to hold processed sentences
+        self.stop_words = open("stop_words.txt", "r", encoding="utf8")
         self.stop = []
-
         self.create_stop_words()
-        print(self.stop)
+        # print(self.stop)
 
         for i in range(46):
             self.current = " " + self.sentences.readline()  # sets current to a line in sentences.txt
@@ -16,11 +17,22 @@ class SentenceProcessor:
             self.current = self.current.lower()  # makes all characters lowercase
             self.current = self.remove_stop_words()  # removes stop words from sentences
             self.current = re.sub(' +', ' ', self.current)  # trims excess spaces
-            self.processed.write(self.current)
+            self.current = self.current.split(' ')
+            self.current.pop(0)
+            self.current.pop(len(self.current) - 1)
+            self.current.insert(len(self.current), "\n")
+            print(self.current)
+            for i in range(len(self.current)):
+                if i < len(self.current) - 1:
+                    line = self.current[i] + " "
+                else:
+                    line = self.current[i]
+                self.processed.write(line)
+
 
     def remove_characters(self):
         line = self.current
-        for char in '0123456789`~!@#$%^&*()-_=+[{]};:,<.>/?':
+        for char in '0123456789`~!@#$%^&*()-_=+[{]};:,<.>/?\'\"\n':
             line = line.replace(char, ' ')
         return line
 
@@ -38,3 +50,7 @@ class SentenceProcessor:
 
 
 sp = SentenceProcessor()
+
+# p = Porter_Stemmer_Python.PorterStemmer()
+# string = "villains".split('')
+# print(p.stem(string, 0, 8))
